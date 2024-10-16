88bc7447b3ec5e1580b476da8e49a1d4f39bdc54
adrianomisina <adriano.misina@gmail.com>
2024-10-16 01:28:09 -0300

    feat: form select

--- src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,23 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './template-driven-forms.component.html',
-  styleUrl: './template-driven-forms.component.scss'
+  styleUrl: './template-driven-forms.component.scss',
 })
 export class TemplateDrivenFormsComponent {
+  public listComidas = signal<Array<{ comida: string; preco: string }>>([
+    {
+      comida: 'x-salada',
+      preco: 'R$10',
+    },
 
+    {
+      comida: 'x-bacon',
+      preco: 'R$11',
+    },
+
+    {
+      comida: 'cochinha',
+      preco: 'R$9',
+    },
+  ]);
 }
